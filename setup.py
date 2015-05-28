82e49c982d2183c2ab7c60220cd4a7a141a289f1
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2015-05-28 23:36:44 +0900

    almost empty library as the first step

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+print find_packages()
+
+setup(
+    name='Kagura',
+    version='0.1',
+    packages=find_packages(),
+
+    description='Machine Learning Library',
+    license='MIT',
+    url='https://github.com/nishio/kagura',
+
+    author='NISHIO Hirokazu',
+    author_email='nishio.hirokazu@gmail.com',
+)

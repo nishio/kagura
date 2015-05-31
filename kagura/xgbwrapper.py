82a5df644a5da1fbae1d6719a18e6a85b766c5e6
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2015-05-31 11:06:10 +0900

    update

--- kagura/xgbwrapper.py
+++ kagura/xgbwrapper.py
@@ -1,61 +1,79 @@
 """
 XGBoost wrapper to fit Scikit Learn interfaces
+
+if 'test' given, do early stopping
+
+
+eta: 0.01[1], 0.4-1.0[2]
+max_depth 9[1], 4-10[2], default 6
+subsample [default=1] 0.4-1[2]
+colsample_bytree [default=1] 0.4-1[2]
+num_round 3000[1] 5..35?[2]
+
+[1]
+https://no2147483647.wordpress.com/2014/09/17/winning-solution-of-kaggle-higgs-competition-what-a-single-model-can-do/
+[2]
+https://www.kaggle.com/c/otto-group-product-classification-challenge/forums/t/14335/1st-place-winner-solution-gilberto-titericz-stanislav-semenov
 """
 
 import xgboost as xgb
+
+
 class XGBWrapper(object):
-    def __init__(self):
-        pass
+    "sklearn adapter"
+    def __init__(self, num_boost_round=400, test=None, **params):
+        self.params = params
+        self.num_boost_round = num_boost_round
+        if test:
+            test_xs, test_ys = test
+            test = xgb.DMatrix(test_xs, label=test_ys)
+        self.test = test
 
     def fit(self, xs, ys):
-        # ys = 1..(C-1)
-        num_class = max(ys)
-        ys = ys - 1
-        #dtrain = xgb.DMatrix(xs[:100], label=ys[:100])
+        # When using Softmax, xgboost requires ys must be in [0, num_class)
+        num_class = self.params['num_class']
+        assert all(0 <= y < num_class for y in ys)
+
         dtrain = xgb.DMatrix(xs, label=ys)
 
         # setup parameters for xgboost
-        param = {}
-        # use softmax multi-class classification
-        param['objective'] = 'multi:softprob'
-        # scale weight of positive examples
-        param['eta'] = 0.1  # 0.01
-        param['max_depth'] = 9
-        param['sub_sample'] = 0.9
-        param['silent'] = 1
-        param['nthread'] = 1
-        param['num_class'] = num_class
-        #param['eval_metric'] = 'auc'
-        param['eval_metric'] = 'merror'
-        self.param = param
-        watchlist = [(dtrain, 'train')]
-        num_boost_round = 400
-        if 1:
-            bst = xgb.train(
-                param, dtrain, num_boost_round=num_boost_round,
-                evals=watchlist,
-            )
-        else:
-            bst = xgb.Booster(param, [dtrain]) # 2nd arg is cache
-            for i in range(num_boost_round):
-                # 2nd arg is custom objective function (obj)
-                bst.update(dtrain, i, None)
-                # 3rd arg is custom evaluation function (feval)
-                bst.eval_set(watchlist, i, None)
+        params = self.params
 
-        #bst.save_model('xgb.model')
+        # use softmax multi-class classification
+        params['objective'] = 'multi:softprob'
+        params['silent'] = 1
+        params['nthread'] = 1
+
+        if not params.get('eval_metric'):
+            params['eval_metric'] = 'mlogloss' # logloss for multiclass
+            # other candidate: auc, merror, ...
+
+        if self.test:
+            watchlist = [(dtrain, 'train'), (self.test, 'test')]
+            early_stopping_rounds = 10
+        else:
+            watchlist = [(dtrain, 'train')]
+            early_stopping_rounds = 0
+
+        bst = xgb.train(
+            params, dtrain, num_boost_round=self.num_boost_round,
+            evals=watchlist,
+            early_stopping_rounds=early_stopping_rounds,
+        )
+
         self.bst = bst
+
         return self
 
     def predict_proba(self, xs):
         print 'predict proba'
         N = xs.shape[0]
-        print N
         xs = xgb.DMatrix(xs)
-        print 'ok'
-        ypred = self.bst.predict(xs)
-        print 'ok2'
-        ypred = ypred.reshape(N, self.param['num_class'])
-        print 'ok3'
+        ypred = self.bst.predict(xs, ntree_limit=self.bst.best_iteration)
+        ypred = ypred.reshape(N, self.params['num_class'])
         return ypred
 
+    def score(self, xs, ys):
+        from sklearn.metrics import log_loss
+        ypred = self.predict_proba(xs)
+        return log_loss(ys, ypred)

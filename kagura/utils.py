6b091c973e887c57b42a25814d1a653e20bbb4c6
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2015-05-31 11:03:19 +0900

    fix

--- kagura/utils.py
+++ kagura/utils.py
@@ -1,3 +1,6 @@
+"""
+utilities
+"""
 
 def second2human(x):
     "given seconds return human-readable string"
@@ -15,15 +18,18 @@ def second2human(x):
 
 import sys
 import time
+import logging
 def start_end_log(f):
-    "decorate function to log when start and end"
-    logging.info("command: %s", " ".join(sys.argv))
-    start_time = time.time()
-    logging.info("start: %s", f)
-    f()
-    logging.info("end: %s", f)
-    elapse = time.time() - start_time
-    logging.info("elapse %s", second2human(elapse))
-    logging.info("command: %s", " ".join(sys.argv))
+    def _wrap():
+        "decorate function to log when start and end"
+        logging.info("command: %s", " ".join(sys.argv))
+        start_time = time.time()
+        logging.info("start: %s", f)
+        f()
+        logging.info("end: %s", f)
+        elapse = time.time() - start_time
+        logging.info("elapse %s", second2human(elapse))
+        logging.info("end command: %s", " ".join(sys.argv))
+    return _wrap
 
 

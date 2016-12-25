a9706eb785f9dd08c205c29e24b93c4689c85ac9
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2016-12-25 17:28:07 +0900

    refine getlogger

--- kagura/getlogger.py
+++ kagura/getlogger.py
@@ -3,20 +3,58 @@ get configured logger
 
 USAGE at the beginning of script:
 
-from kagura.getlogger import logging
+(obsolete)from kagura.getlogger import logging
+
+It is obsolete because it is not good practice.
+
+
 """
 
-import logging
-import sys
-import atexit
+from logging import getLogger, StreamHandler, DEBUG
 
-logging.basicConfig(
-    filename='log.txt',level=logging.DEBUG,
-    format='%(asctime)s/(%(process)d) %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
-)
-logging.info("start process: %s", " ".join(sys.argv))
+def get_logger_to_file():
+    import logging
+    logging.basicConfig(
+        filename='log.txt',level=logging.DEBUG,
+        format='%(asctime)s/(%(process)d) %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    return getLogger("kagura")
+logger = get_logger_to_file()  # it write on file silently
 
-def end():
-    logging.info("end process: %s", " ".join(sys.argv))
 
-atexit.register(end)
+def get_logger_to_stderr():
+    logger = getLogger("kagura.stderr")
+    handler = StreamHandler()
+    handler.setLevel(DEBUG)
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler)
+    return logger  # it write on file and stderr
+
+
+def log_process_life():
+    import sys
+    import atexit
+    import logging
+    logging.info("start process: %s", " ".join(sys.argv))
+
+    def end():
+        logging.info("end process: %s", " ".join(sys.argv))
+
+    atexit.register(end)
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+    logger =  get_logger_to_file()
+    log_process_life()
+    logger.debug('hello')
+
+    logger2 =  get_logger_to_stderr()
+    logger2.debug('hello again')
+
+    logger.debug('hello three')
+
+
+if __name__ == '__main__':
+    _test()
